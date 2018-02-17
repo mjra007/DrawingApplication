f809dd79b84b614766e4343485c7e891329acdee
Micael Araujo <micael12jose@gmail.com>
2018-02-17 15:51:45 +0000

    Improved Shape detection recognition

--- src/simpledrawer/Container.java
+++ src/simpledrawer/Container.java
@@ -1,17 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package simpledrawer;
-
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.util.List;
-
 /**
  *
  * @author ma8521e
@@ -25,32 +11,54 @@ import java.util.List;
  *
  * ########## # # # Entity # # # ##########
  */
+package simpledrawer;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.List;
+
 public class Container implements DrawableI, InteractiveShape {
 
     //Points required to draw the shape contained
-    Entity entityContained=null;
-    Point containerOrigin = null;
-    Integer containerHeight=null;
-    Integer containerWidth=null;
+    Entity contained = null;
     boolean selected = false;
     final static float dash1[] = {10.0f};
     final static BasicStroke dashed = new BasicStroke(2.0f,
             BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f);
 
-    public Container(Entity entity) {
-        this.entityContained = entity;
-        List<Point> reorganizedCoords = Utils.getReorganizedCoords(entityContained.getStructuralPoints().get(0), entityContained.getStructuralPoints().get(1));
-        this.containerOrigin = new Point(reorganizedCoords.get(0).x-5,reorganizedCoords.get(0).y-5);  
-        this.containerHeight = (reorganizedCoords.get(1).y-reorganizedCoords.get(0).y) + 10;
-        this.containerWidth = (reorganizedCoords.get(1).x-reorganizedCoords.get(0).x) + 10;
+    public Container(Entity contained) {
+        this.contained = contained;
+    }
+
+    //returns the reorganized coords for the container
+    public List<Point> getReorganizedCoords() {
+        return Utils.getReorganizedCoords(contained.getStructuralPoints().get(0), contained.getStructuralPoints().get(1));
+    }
+
+    public Point getOrigin() {
+        return new Point(getReorganizedCoords().get(0).x - 5, getReorganizedCoords().get(0).y - 5);
+    }
+
+    public int getWidth() {
+        return (getReorganizedCoords().get(1).x - getReorganizedCoords().get(0).x) + 10;
+    }
+
+    public int getHeight() {
+        return (getReorganizedCoords().get(1).y - getReorganizedCoords().get(0).y) + 10;
+    }
+
+    public Point getEndPoint() {
+        return new Point(getWidth() + getOrigin().x, getHeight() + getOrigin().y);
     }
 
     @Override
     public boolean contains(Point p) {
         boolean doesIt = false;
         Rectangle rectangle = new Rectangle();
-        rectangle.setSize(containerHeight, containerWidth);
-        rectangle.setLocation(containerOrigin);
+        rectangle.setFrameFromDiagonal(getOrigin(), getEndPoint());
         if (rectangle.contains(p)) {
             doesIt = true;
         }
@@ -59,18 +67,42 @@ public class Container implements DrawableI, InteractiveShape {
 
     @Override
     public Container translate(List<Point> old, Point offset) {
-        int offsetX = offset.x;
-        int offsetY = offset.y;
         for (int i = 0; i < old.size(); i++) {
-            Point point = old.get(i);
-            this.entityContained.structuralPoints.set(i, new Point(point.x + offsetX +5, point.y + offsetY +5));
+            Point oldPoint = old.get(i);
+            this.contained.getStructuralPoints().set(i, new Point(oldPoint.x + offset.x , oldPoint.y + offset.y));
         }
-        this.containerOrigin = new Point(old.get(0).x + offsetX, old.get(0).y + offsetY);
         return this;
     }
 
     @Override
     public Container resize(Point offSet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsBottomRightCorner(Point p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    @Override
+    public boolean containsBottomLeftCorner(Point p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsTopRightCorner(Point p) {
+        boolean doesIt = false;
+        Rectangle rectangle = new Rectangle();
+        rectangle.setFrameFromDiagonal(new Point(getOrigin().x-10, getOrigin().y-10),new Point(getOrigin().x+10, getOrigin().y+10));
+        if (rectangle.contains(p)) {
+            doesIt = true;
+        }
+        return doesIt;
+    }
+
+    @Override
+    public boolean containsTopLeftCorner(Point p) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -82,11 +114,11 @@ public class Container implements DrawableI, InteractiveShape {
     }
 
     public void contain(Entity entity) {
-        this.entityContained = entity;
+        this.contained = entity;
     }
 
     public Entity getContained() {
-        return this.entityContained;
+        return this.contained;
     }
 
     public boolean Select() {
@@ -99,20 +131,24 @@ public class Container implements DrawableI, InteractiveShape {
 
     public Rectangle getDrawableContainer() {
         Rectangle rect = new Rectangle();
-        rect.setLocation(containerOrigin);
-        rect.height = containerHeight;
-        rect.width = containerWidth;
+        rect.setLocation(getOrigin());
+        rect.height = getHeight();
+        rect.width = getWidth();
         return rect;
     }
 
+  /*  public  getDrawableCorner(Point origin){
+        
+    }*/
+    
     @Override
     public void drawShape(Graphics2D g2d, float currentBrightness) {
         g2d.setStroke(dashed);
         g2d.setColor(Color.blue);
-        if(selected){
-        g2d.draw(getDrawableContainer());
+        if (selected) {
+            g2d.draw(getDrawableContainer());
         }
-        entityContained.drawShape(g2d, currentBrightness);
+        contained.drawShape(g2d, currentBrightness);
     }
-   
+
 }
